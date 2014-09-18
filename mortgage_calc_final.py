d222fcf9ee17bb800683d6188d410993ffad8766
sp00f <e.cudjoe@hotmail.co.uk>
2014-09-18 19:08:48 +0100

    added main()

--- mortgage_calc_final.py
+++ mortgage_calc_final.py
@@ -44,65 +44,69 @@ class Calc(object):
         total_interest_paid = fnc - loan
         print("The total amount of interest paid over {} years: £{:,.2f}."
                 .format(self.term, total_interest_paid))
-    	
-value_store = {
-    'value': None,
-    'deposit': None,
-    'interest': None,
-    'term': None
-    }
 
-questions = True
-while questions:
-    try:
-        if not value_store['value']:
-            value_store['value'] = int(raw_input(
-                "Enter the property value (£): ").replace(',', ''))
-        if not value_store['deposit']:
-            value_store['deposit'] = int(raw_input(
-                "Enter your deposit amount (£): ").replace(',', ''))
-        if not value_store['interest']:
-            value_store['interest'] = float(raw_input(
-                "Enter your bank's interest rate (%): "))
-        if not value_store['term']:
-            value_store['term'] = int(raw_input(
-                "Enter the length of mortgage term (years): "))
-        
-        user = Calc(**value_store)
+def main():    	
+    value_store = {
+        'value': None,
+        'deposit': None,
+        'interest': None,
+        'term': None
+        }
 
-             
-        print("select {0:1} to calculate your monthly mortgage payments"
-            .format(1))
-        print("select {0:1} to calculate the total amount you will pay."
-            .format(2))
-        print("select {0:1} to calculate the total interest you will pay"
-            .format(3))
-        print("select {0:1} to quit".format("q"))
-        
-        userchoice = raw_input("please select from above: ")
+    questions = True
+    while questions:
+        try:
+            if not value_store['value']:
+                value_store['value'] = int(raw_input(
+                    "Enter the property value (£): ").replace(',', ''))
+            if not value_store['deposit']:
+                value_store['deposit'] = int(raw_input(
+                    "Enter your deposit amount (£): ").replace(',', ''))
+            if not value_store['interest']:
+                value_store['interest'] = float(raw_input(
+                    "Enter your bank's interest rate (%): "))
+            if not value_store['term']:
+                value_store['term'] = int(raw_input(
+                    "Enter the length of mortgage term (years): "))
+            
+            user = Calc(**value_store)
 
-        if userchoice == "1":
-            user.monthly_payments()
-        elif userchoice == "2":
-            user.total_amount_paid()
-        elif userchoice == "3":
-            user.total_interest_paid(user.total_amount_paid())
-        elif userchoice == "q":
-            questions = False
-        else:
-            print("I dont have that option available.")
-        
-        
-        end_calc = raw_input("Would you like to continue? [y/n]: ")
-        if end_calc == "n":
-            print("Goodbye") 
-            questions = False
-        elif end_calc == "y":
-            questions = True
-        else:
-            questions = True
-    except ValueError:
-        print("Oops! I expected a number there.") 
+                 
+            print("select {0:1} to calculate your monthly mortgage payments"
+                .format(1))
+            print("select {0:1} to calculate the total amount you will pay."
+                .format(2))
+            print("select {0:1} to calculate the total interest you will pay"
+                .format(3))
+            print("select {0:1} to quit".format("q"))
+            
+            userchoice = raw_input("please select from above: ")
+
+            if userchoice == "1":
+                user.monthly_payments()
+            elif userchoice == "2":
+                user.total_amount_paid()
+            elif userchoice == "3":
+                user.total_interest_paid(user.total_amount_paid())
+            elif userchoice == "q":
+                questions = False
+            else:
+                print("I dont have that option available.")
+            
+            
+            end_calc = raw_input("Would you like to continue? [y/n]: ")
+            if end_calc == "n":
+                print("Goodbye") 
+                questions = False
+            elif end_calc == "y":
+                questions = True
+            else:
+                questions = True
+        except ValueError:
+            print("Oops! I expected a number there.") 
+
+if __name__ == '__main__':
+    main()
 
 
 
